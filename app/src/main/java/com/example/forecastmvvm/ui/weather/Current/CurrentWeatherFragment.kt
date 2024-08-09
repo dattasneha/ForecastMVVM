8cb1cb0d92f5fc841da25f9b94da18640ae8a3e9
Sneha Datta <snehadatta431@gmail.com>
2024-08-09 12:01:19 +0530

    Setup Retrofit

--- app/src/main/java/com/example/forecastmvvm/ui/weather/Current/CurrentWeatherFragment.kt
+++ app/src/main/java/com/example/forecastmvvm/ui/weather/Current/CurrentWeatherFragment.kt
@@ -6,7 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.forecastmvvm.R
+import com.example.forecastmvvm.data.ApixuWeatherApiService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class CurrentWeatherFragment : Fragment() {
 
@@ -21,12 +26,20 @@ class CurrentWeatherFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_current_weather, container, false)
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(CurrentWeatherViewModel::class.java)
         // TODO: Use the ViewModel
+        val apiService = ApixuWeatherApiService()
+
+        GlobalScope.launch(Dispatchers.Main){
+            val currentWeatherResponse = apiService.getCurrentWeather("Kolkata", "no").await()
+            val textview =requireView().findViewById<TextView>(R.id.textView)
+            textview.text= currentWeatherResponse.currentWeatherEntry.toString()
+        }
     }
 
 }
